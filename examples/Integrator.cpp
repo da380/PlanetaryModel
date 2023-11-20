427cb84a16fcdcc5fe00b170ed4d4c026ddd6e6d
Alex Myhill <adcm2@cam.ac.uk>
2023-11-20 20:02:04 +0000

    Something

--- examples/Integrator.cpp
+++ examples/Integrator.cpp
@@ -1,73 +1,94 @@
-#include <PlanetaryModel/All>
 #include <GaussQuad/All>
+#include <PlanetaryModel/All>
 #include <concepts>
 #include <iostream>
 
 #include "PREM.h"
 
+int
+main() {
+    using Float = double;
 
+    using namespace GaussQuad;
 
-int main() {
-  using Float = double;
+    // Set the output precision
+    using std::cout;
+    using std::endl;
+    cout.setf(std::ios_base::scientific);
+    cout.setf(std::ios_base::showpos);
+    cout.precision(16);
 
-  using namespace GaussQuad;
+    // Set the floating point precision
+    using Float = double;
 
-  // Set the output precision
-  using std::cout;
-  using std::endl;
-  cout.setf(std::ios_base::scientific);
-  cout.setf(std::ios_base::showpos);
-  cout.precision(16);
+    // Build the quadrature
+    int n = 5;
+    auto q = GaussLobattoLegendreQuadrature1D<Float>(n);
 
-  // Set the floating point precision
-  using Float = double;
+    // write out the points and weights
+    for (int i = 0; i < n; i++) {
+        std::cout << q.X(i) << " " << q.W(i) << std::endl;
+    }
 
-  // Build the quadrature
-  int n = 5;
-  auto q = GaussLobattoLegendreQuadrature1D<Float>(n);
+    // define a simple function to integrate
+    auto fun = [](Float x) { return x * x; };
 
-  // write out the points and weights
-  for (int i = 0; i < n; i++) {
-    std::cout << q.X(i) << " " << q.W(i) << std::endl;
-  }
+    // set the exact value for the integral
+    Float exact = Float(2.0) / Float(3.0);
 
-  // define a simple function to integrate
-  auto fun = [](Float x) { return x * x; };
+    cout << "Numerical value = " << q.Integrate(fun)
+         << ", exact value = " << exact << endl;
 
-  // set the exact value for the integral
-  Float exact = Float(2.0) / Float(3.0);
-
-  cout << "Numerical value = " << q.Integrate(fun)
-       << ", exact value = " << exact << endl;
-
-    //assertions of concept for model 
+    // assertions of concept for model
     static_assert(
-      PlanetaryModel::HasNormalisationInformation<EarthConstants<double> >);
-  static_assert(PlanetaryModel::SphericalGeometryModel<PREM<double, int> >);
-  static_assert(PlanetaryModel::SphericalDensityModel<PREM<double, int> >);
-  static_assert(PlanetaryModel::SphericalElasticModel<PREM<double, int> >);
+        PlanetaryModel::HasNormalisationInformation<EarthConstants<double>>);
+    static_assert(PlanetaryModel::SphericalGeometryModel<PREM<double, int>>);
+    static_assert(PlanetaryModel::SphericalDensityModel<PREM<double, int>>);
+    static_assert(PlanetaryModel::SphericalElasticModel<PREM<double, int>>);
 
-  //finding mass and moment of inertia of a spherical model
-  auto myprem = PREM();
-    double PMass = 0; 
+    // finding mass and moment of inertia of a spherical model
+    auto myprem = PREM();
+    double PMass = 0;
     double PINIT = 0;
     int numlayers = myprem.NumberOfLayers();
-    auto nmap = [](Float xi, Float x1, Float x2) {return 0.5 * ((x1 + x2) + (x2 - x1) * xi);};
-    auto dval = [&myprem, nmap](Float x, int i){return myprem.Density(i)(nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i))/myprem.OuterRadius())  * nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)) * nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)) ;};
-    auto IVAL = [&myprem, nmap](Float x, int i){return myprem.Density(i)(nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i))/myprem.OuterRadius())  * std::pow(nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)),4);};
-    
-    std::cout << "Hello: " << nmap(-1,0,10) << " " << nmap(1,0,10) << std::endl;
-    std::cout << "Hello: " << dval(-1.0,0) << " " << dval(1.0,0) << std::endl;
-    std::cout << myprem.Density(0)(nmap(-1, myprem.LowerRadius(0), myprem.UpperRadius(0))) << std::endl;
+    auto nmap = [](Float xi, Float x1, Float x2) {
+        return 0.5 * ((x1 + x2) + (x2 - x1) * xi);
+    };
+    auto dval = [&myprem, nmap](Float x, int i) {
+        return myprem.Density(i)(
+                   nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)) /
+                   myprem.OuterRadius()) *
+               nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)) *
+               nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i));
+    };
+    auto IVAL = [&myprem, nmap](Float x, int i) {
+        return myprem.Density(i)(
+                   nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)) /
+                   myprem.OuterRadius()) *
+               std::pow(nmap(x, myprem.LowerRadius(i), myprem.UpperRadius(i)),
+                        4);
+    };
 
-    //loop over all layers
-    for (int idx = 0; idx < myprem.NumberOfLayers(); ++idx){
-        auto lval = [dval, idx](Float x){return dval(x, idx);};
-        PMass += 4.0 * 3.1415926535 * 0.5 * (myprem.UpperRadius(idx) - myprem.LowerRadius(idx)) * q.Integrate(lval);
-        auto lval2 = [IVAL, idx](Float x){return IVAL(x, idx);};
-        PINIT += 8.0 * 3.1415926535 /3 * 0.5 * (myprem.UpperRadius(idx) - myprem.LowerRadius(idx)) * q.Integrate(lval2);
+    std::cout << "Hello: " << nmap(-1, 0, 10) << " " << nmap(1, 0, 10)
+              << std::endl;
+    std::cout << "Hello: " << dval(-1.0, 0) << " " << dval(1.0, 0) << std::endl;
+    std::cout << myprem.Density(0)(
+                     nmap(-1, myprem.LowerRadius(0), myprem.UpperRadius(0)))
+              << std::endl;
+
+    // loop over all layers
+    for (int idx = 0; idx < myprem.NumberOfLayers(); ++idx) {
+        auto lval = [dval, idx](Float x) { return dval(x, idx); };
+        PMass += 1000.0 * 4.0 * 3.1415926535 * 0.5 *
+                 (myprem.UpperRadius(idx) - myprem.LowerRadius(idx)) *
+                 q.Integrate(lval);
+        auto lval2 = [IVAL, idx](Float x) { return IVAL(x, idx); };
+        PINIT += 1000.0 * 8.0 * 3.1415926535 / 3 * 0.5 *
+                 (myprem.UpperRadius(idx) - myprem.LowerRadius(idx)) *
+                 q.Integrate(lval2);
     }
-    std::cout << "The mass of the Earth in PREM is: " << PMass << " kg" << std::endl;
-std::cout << "The moment of inertia of the Earth in PREM is: " << PINIT << std::endl;
-
+    std::cout << "The mass of the Earth in PREM is: " << PMass << " kg"
+              << std::endl;
+    std::cout << "The moment of inertia of the Earth in PREM is: " << PINIT
+              << std::endl;
 }
--- examples/PREM.h
+++ examples/PREM.h
@@ -11,274 +11,273 @@
 
 #include "Interpolation/All"
 
-template <typename FLOAT>
-class EarthConstants {
- public:
-  using value_type = FLOAT;
-  FLOAT LengthNorm() { return length_norm; };
-  FLOAT MassNorm() { return mass_norm; };
-  FLOAT TimeNorm() { return time_norm; }
+template <typename FLOAT> class EarthConstants {
+  public:
+    using value_type = FLOAT;
+    FLOAT LengthNorm() { return length_norm; };
+    FLOAT MassNorm() { return mass_norm; };
+    FLOAT TimeNorm() { return time_norm; }
 
-  FLOAT DensityNorm() { return density_norm; };
-  FLOAT InertiaNorm() { return inertia_norm; };
-  FLOAT VelocityNorm() { return velocity_norm; };
-  FLOAT AccelerationNorm() { return acceleration_norm; };
-  FLOAT ForceNorm() { return force_norm; };
-  FLOAT StressNorm() { return stress_norm; };
-  FLOAT GravitationalConstant() { return gravitational_constant; };
+    FLOAT DensityNorm() { return density_norm; };
+    FLOAT InertiaNorm() { return inertia_norm; };
+    FLOAT VelocityNorm() { return velocity_norm; };
+    FLOAT AccelerationNorm() { return acceleration_norm; };
+    FLOAT ForceNorm() { return force_norm; };
+    FLOAT StressNorm() { return stress_norm; };
+    FLOAT GravitationalConstant() { return gravitational_constant; };
 
- private:
-  const FLOAT length_norm = 6.371 * std::pow(10.0, 6.0);
-  const FLOAT mass_norm = 5.972 * std::pow(10.0, 24.0);
-  const FLOAT time_norm = 3600.0;
+  private:
+    const FLOAT length_norm = 6.371 * std::pow(10.0, 6.0);
+    const FLOAT mass_norm = 5.972 * std::pow(10.0, 24.0);
+    const FLOAT time_norm = 3600.0;
 
-  const FLOAT density_norm = mass_norm / std::pow(length_norm, 3.0);
-  const FLOAT inertia_norm = mass_norm * std::pow(length_norm, 2.0);
-  const FLOAT velocity_norm = length_norm / time_norm;
-  const FLOAT acceleration_norm = length_norm / std::pow(time_norm, 2.0);
-  const FLOAT force_norm = mass_norm * length_norm / std::pow(time_norm, 2.0);
-  const FLOAT stress_norm =
-      mass_norm / (std::pow(time_norm, 2.0) * length_norm);
-  const FLOAT gravitational_constant =
-      std::pow(length_norm, 3.0) / (mass_norm * std::pow(time_norm, 2.0));
+    const FLOAT density_norm = mass_norm / std::pow(length_norm, 3.0);
+    const FLOAT inertia_norm = mass_norm * std::pow(length_norm, 2.0);
+    const FLOAT velocity_norm = length_norm / time_norm;
+    const FLOAT acceleration_norm = length_norm / std::pow(time_norm, 2.0);
+    const FLOAT force_norm = mass_norm * length_norm / std::pow(time_norm, 2.0);
+    const FLOAT stress_norm =
+        mass_norm / (std::pow(time_norm, 2.0) * length_norm);
+    const FLOAT gravitational_constant =
+        std::pow(length_norm, 3.0) / (mass_norm * std::pow(time_norm, 2.0));
 };
 
 template <typename FLOAT = double, typename INTEGRAL = int>
 class PREM : public EarthConstants<FLOAT> {
- public:
-  using size_type = INTEGRAL;
+  public:
+    using size_type = INTEGRAL;
 
-  // Constructor
-  PREM(){};
+    // Constructor
+    PREM(){};
 
-  // Geometry of PREM
-  INTEGRAL NumberOfLayers() { return 13; };
-  FLOAT LowerRadius(INTEGRAL i) { return vec_radii[i]; }
-  FLOAT UpperRadius(INTEGRAL i) { return vec_radii[i + 1]; }
-  FLOAT OuterRadius() { return vec_radii[13]; }
+    // Geometry of PREM
+    INTEGRAL NumberOfLayers() { return 13; };
+    FLOAT LowerRadius(INTEGRAL i) { return vec_radii[i]; }
+    FLOAT UpperRadius(INTEGRAL i) { return vec_radii[i + 1]; }
+    FLOAT OuterRadius() { return vec_radii[13]; }
 
-  // Density
-  Interpolation::Polynomial1D<FLOAT> Density(INTEGRAL i) {
-    return vec_density[i];
-  };
+    // Density
+    Interpolation::Polynomial1D<FLOAT> Density(INTEGRAL i) {
+        return vec_density[i];
+    };
 
-  // Isotropy/fluid/solid etc
-  bool IsIsotropic() { return false; };
+    // Isotropy/fluid/solid etc
+    bool IsIsotropic() { return false; };
 
-  // Solid or fluid
-  bool IsSolid(INTEGRAL i) {
-    if (i == 1 || i == 12) {
-      return false;
-    } else {
-      return true;
+    // Solid or fluid
+    bool IsSolid(INTEGRAL i) {
+        if (i == 1 || i == 12) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
-  bool IsFluid(INTEGRAL i) { return !IsSolid(i); }
+    bool IsFluid(INTEGRAL i) { return !IsSolid(i); }
 
-  // Return TI elastic modulii
+    // Return TI elastic modulii
 
-  // Velocities
+    // Velocities
 
-  Interpolation::Polynomial1D<FLOAT> VP(INTEGRAL i) {
-    return vec_p_velocity[i];
-  };
-  Interpolation::Polynomial1D<FLOAT> VPV(INTEGRAL i) {
-    return vec_pv_velocity[i];
-  };
-  Interpolation::Polynomial1D<FLOAT> VPH(INTEGRAL i) {
-    return vec_ph_velocity[i];
-  };
-  Interpolation::Polynomial1D<FLOAT> VS(INTEGRAL i) {
-    return vec_s_velocity[i];
-  };
-  Interpolation::Polynomial1D<FLOAT> VSV(INTEGRAL i) {
-    return vec_sv_velocity[i];
-  };
-  Interpolation::Polynomial1D<FLOAT> VSH(INTEGRAL i) {
-    return vec_sh_velocity[i];
-  };
-
-  // Returning eta, A, C, N, L, kappa, mu
-  auto Eta(INTEGRAL i) { return vec_eta[i]; }
-  auto A(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return Density(i)(x) * VPH(i)(x) * VPH(i)(x);
+    Interpolation::Polynomial1D<FLOAT> VP(INTEGRAL i) {
+        return vec_p_velocity[i];
     };
-    return aret;
-  };
-  auto C(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return Density(i)(x) * VPV(i)(x) * VPV(i)(x);
+    Interpolation::Polynomial1D<FLOAT> VPV(INTEGRAL i) {
+        return vec_pv_velocity[i];
     };
-    return aret;
-  };
-  auto N(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return Density(i)(x) * VSH(i)(x) * VSH(i)(x);
+    Interpolation::Polynomial1D<FLOAT> VPH(INTEGRAL i) {
+        return vec_ph_velocity[i];
     };
-    return aret;
-  };
-  auto L(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return Density(i)(x) * VSV(i)(x) * VSV(i)(x);
+    Interpolation::Polynomial1D<FLOAT> VS(INTEGRAL i) {
+        return vec_s_velocity[i];
     };
-    return aret;
-  };
-  auto F(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return Eta(i)(x) * (A(i)(x) - 2 * L(i)(x));
+    Interpolation::Polynomial1D<FLOAT> VSV(INTEGRAL i) {
+        return vec_sv_velocity[i];
     };
-    return aret;
-  };
-  auto Kappa(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return (C(i)(x) + 4.0 * (A(i)(x) - N(i)(x) + F(i)(x))) / 9.0;
+    Interpolation::Polynomial1D<FLOAT> VSH(INTEGRAL i) {
+        return vec_sh_velocity[i];
     };
-    return aret;
-  };
-  auto Mu(INTEGRAL i) {
-    auto aret = [i, this](FLOAT x) {
-      return (C(i)(x) + A(i)(x) + 6.0 * L(i)(x) + 5.0 * N(i)(x) -
-              2.0 * F(i)(x)) /
-             15.0;
+
+    // Returning eta, A, C, N, L, kappa, mu
+    auto Eta(INTEGRAL i) { return vec_eta[i]; }
+    auto A(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return Density(i)(x) * VPH(i)(x) * VPH(i)(x);
+        };
+        return aret;
     };
-    return aret;
-  };
+    auto C(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return Density(i)(x) * VPV(i)(x) * VPV(i)(x);
+        };
+        return aret;
+    };
+    auto N(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return Density(i)(x) * VSH(i)(x) * VSH(i)(x);
+        };
+        return aret;
+    };
+    auto L(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return Density(i)(x) * VSV(i)(x) * VSV(i)(x);
+        };
+        return aret;
+    };
+    auto F(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return Eta(i)(x) * (A(i)(x) - 2 * L(i)(x));
+        };
+        return aret;
+    };
+    auto Kappa(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return (C(i)(x) + 4.0 * (A(i)(x) - N(i)(x) + F(i)(x))) / 9.0;
+        };
+        return aret;
+    };
+    auto Mu(INTEGRAL i) {
+        auto aret = [i, this](FLOAT x) {
+            return (C(i)(x) + A(i)(x) + 6.0 * L(i)(x) + 5.0 * N(i)(x) -
+                    2.0 * F(i)(x)) /
+                   15.0;
+        };
+        return aret;
+    };
 
-  // data
- private:
-  std::vector<FLOAT> vec_radii{0.0,       1221500.0, 3480000.0, 3630000.0,
-                               5600000.0, 5701000.0, 5771000.0, 5971000.0,
-                               6151000.0, 6291000.0, 6346600.0, 6356000.0,
-                               6368000.0, 6371000.0};
+    // data
+  private:
+    std::vector<FLOAT> vec_radii{0.0,       1221500.0, 3480000.0, 3630000.0,
+                                 5600000.0, 5701000.0, 5771000.0, 5971000.0,
+                                 6151000.0, 6291000.0, 6346600.0, 6356000.0,
+                                 6368000.0, 6371000.0};
 
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_density{
-      {13.0855, 0, -8.8381},
-      {12.5815, -1.2638, -3.6426, -5.5281},
-      {7.9565, -6.4761, 5.5283, -3.0807},
-      {7.9565, -6.4761, 5.5283, -3.0807},
-      {7.9565, -6.4761, 5.5283, -3.0807},
-      {5.3197, -1.4836},
-      {11.2494, -8.0298},
-      {7.1089, -3.8045},
-      {2.6910, 0.6924},
-      {2.6910, 0.6924},
-      {2.900},
-      {2.600},
-      {1.020}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_density{
+        {13.0855, 0, -8.8381},
+        {12.5815, -1.2638, -3.6426, -5.5281},
+        {7.9565, -6.4761, 5.5283, -3.0807},
+        {7.9565, -6.4761, 5.5283, -3.0807},
+        {7.9565, -6.4761, 5.5283, -3.0807},
+        {5.3197, -1.4836},
+        {11.2494, -8.0298},
+        {7.1089, -3.8045},
+        {2.6910, 0.6924},
+        {2.6910, 0.6924},
+        {2.900},
+        {2.600},
+        {1.020}};
 
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_p_velocity{
-      {11.2622, 0, -6.3640},
-      {11.0487, -4.0362, 4.8023, -13.5732},
-      {15.3891, -5.3181, 5.5242, -2.5514},
-      {24.9520, -40.4673, 51.4832, -26.6419},
-      {29.2766, -23.6027, 5.5242, -2.5514},
-      {19.0957, -9.8672},
-      {39.7027, -32.6166},
-      {20.3926, -12.2569},
-      {4.1875, 3.9382},
-      {4.1875, 3.9382},
-      {6.800},
-      {5.800},
-      {1.450}};
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_pv_velocity{
-      {11.2622, 0, -6.3640},
-      {11.0487, -4.0362, 4.8023, -13.5732},
-      {15.3891, -5.3181, 5.5242, -2.5514},
-      {24.9520, -40.4673, 51.4832, -26.6419},
-      {29.2766, -23.6027, 5.5242, -2.5514},
-      {19.0957, -9.8672},
-      {39.7027, -32.6166},
-      {20.3926, -12.2569},
-      {0.8317, 7.2180},
-      {0.8317, 7.2180},
-      {6.800},
-      {5.800},
-      {1.450}};
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_ph_velocity{
-      {11.2622, 0, -6.3640},
-      {11.0487, -4.0362, 4.8023, -13.5732},
-      {15.3891, -5.3181, 5.5242, -2.5514},
-      {24.9520, -40.4673, 51.4832, -26.6419},
-      {29.2766, -23.6027, 5.5242, -2.5514},
-      {19.0957, -9.8672},
-      {39.7027, -32.6166},
-      {20.3926, -12.2569},
-      {3.5908, 4.6172},
-      {3.5908, 4.6172},
-      {6.800},
-      {5.800},
-      {1.450}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_p_velocity{
+        {11.2622, 0, -6.3640},
+        {11.0487, -4.0362, 4.8023, -13.5732},
+        {15.3891, -5.3181, 5.5242, -2.5514},
+        {24.9520, -40.4673, 51.4832, -26.6419},
+        {29.2766, -23.6027, 5.5242, -2.5514},
+        {19.0957, -9.8672},
+        {39.7027, -32.6166},
+        {20.3926, -12.2569},
+        {4.1875, 3.9382},
+        {4.1875, 3.9382},
+        {6.800},
+        {5.800},
+        {1.450}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_pv_velocity{
+        {11.2622, 0, -6.3640},
+        {11.0487, -4.0362, 4.8023, -13.5732},
+        {15.3891, -5.3181, 5.5242, -2.5514},
+        {24.9520, -40.4673, 51.4832, -26.6419},
+        {29.2766, -23.6027, 5.5242, -2.5514},
+        {19.0957, -9.8672},
+        {39.7027, -32.6166},
+        {20.3926, -12.2569},
+        {0.8317, 7.2180},
+        {0.8317, 7.2180},
+        {6.800},
+        {5.800},
+        {1.450}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_ph_velocity{
+        {11.2622, 0, -6.3640},
+        {11.0487, -4.0362, 4.8023, -13.5732},
+        {15.3891, -5.3181, 5.5242, -2.5514},
+        {24.9520, -40.4673, 51.4832, -26.6419},
+        {29.2766, -23.6027, 5.5242, -2.5514},
+        {19.0957, -9.8672},
+        {39.7027, -32.6166},
+        {20.3926, -12.2569},
+        {3.5908, 4.6172},
+        {3.5908, 4.6172},
+        {6.800},
+        {5.800},
+        {1.450}};
 
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_s_velocity{
-      {3.6678, 0, -4.4475},
-      {0.0},
-      {6.9254, 1.4672, -2.0834, 0.9783},
-      {11.1671, -13.7818, 17.4575, -9.2777},
-      {22.3459, -17.2473, -2.0834, 0.9783},
-      {9.9839, -4.9324},
-      {22.3512, -18.5856},
-      {8.9496, -4.4597},
-      {2.1519, 2.3481},
-      {2.1519, 2.3481},
-      {3.900},
-      {3.200},
-      {0}};
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_sv_velocity{
-      {3.6678, 0, -4.4475},
-      {0.0},
-      {6.9254, 1.4672, -2.0834, 0.9783},
-      {11.1671, -13.7818, 17.4575, -9.2777},
-      {22.3459, -17.2473, -2.0834, 0.9783},
-      {9.9839, -4.9324},
-      {22.3512, -18.5856},
-      {8.9496, -4.4597},
-      {5.8582, -1.4678},
-      {5.8582, -1.4678},
-      {3.900},
-      {3.200},
-      {0}};
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_sh_velocity{
-      {3.6678, 0, -4.4475},
-      {0.0},
-      {6.9254, 1.4672, -2.0834, 0.9783},
-      {11.1671, -13.7818, 17.4575, -9.2777},
-      {22.3459, -17.2473, -2.0834, 0.9783},
-      {9.9839, -4.9324},
-      {22.3512, -18.5856},
-      {8.9496, -4.4597},
-      {-1.0839, 5.7176},
-      {-1.0839, 5.7176},
-      {3.900},
-      {3.200},
-      {0}};
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_Qmu{
-      {84.6}, {std::pow(10.0, 10.0)},
-      {312},  {312},
-      {312},  {143},
-      {143},  {143},
-      {80},   {600},
-      {600},  {std::pow(10.0, 10.0)}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_s_velocity{
+        {3.6678, 0, -4.4475},
+        {0.0},
+        {6.9254, 1.4672, -2.0834, 0.9783},
+        {11.1671, -13.7818, 17.4575, -9.2777},
+        {22.3459, -17.2473, -2.0834, 0.9783},
+        {9.9839, -4.9324},
+        {22.3512, -18.5856},
+        {8.9496, -4.4597},
+        {2.1519, 2.3481},
+        {2.1519, 2.3481},
+        {3.900},
+        {3.200},
+        {0}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_sv_velocity{
+        {3.6678, 0, -4.4475},
+        {0.0},
+        {6.9254, 1.4672, -2.0834, 0.9783},
+        {11.1671, -13.7818, 17.4575, -9.2777},
+        {22.3459, -17.2473, -2.0834, 0.9783},
+        {9.9839, -4.9324},
+        {22.3512, -18.5856},
+        {8.9496, -4.4597},
+        {5.8582, -1.4678},
+        {5.8582, -1.4678},
+        {3.900},
+        {3.200},
+        {0}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_sh_velocity{
+        {3.6678, 0, -4.4475},
+        {0.0},
+        {6.9254, 1.4672, -2.0834, 0.9783},
+        {11.1671, -13.7818, 17.4575, -9.2777},
+        {22.3459, -17.2473, -2.0834, 0.9783},
+        {9.9839, -4.9324},
+        {22.3512, -18.5856},
+        {8.9496, -4.4597},
+        {-1.0839, 5.7176},
+        {-1.0839, 5.7176},
+        {3.900},
+        {3.200},
+        {0}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_Qmu{
+        {84.6}, {std::pow(10.0, 10.0)},
+        {312},  {312},
+        {312},  {143},
+        {143},  {143},
+        {80},   {600},
+        {600},  {std::pow(10.0, 10.0)}};
 
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_QKappa{
-      {1327.7}, {57823}, {57823}, {57823}, {57823}, {57823},
-      {57823},  {57823}, {57823}, {57823}, {57823}, {57823}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_QKappa{
+        {1327.7}, {57823}, {57823}, {57823}, {57823}, {57823},
+        {57823},  {57823}, {57823}, {57823}, {57823}, {57823}};
 
-  std::vector<Interpolation::Polynomial1D<FLOAT>> vec_eta{{1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {1},
-                                                          {3.3687, -2.4778},
-                                                          {3.3687, -2.4778},
-                                                          {1},
-                                                          {1},
-                                                          {1}};
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_eta{{1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {1},
+                                                            {3.3687, -2.4778},
+                                                            {3.3687, -2.4778},
+                                                            {1},
+                                                            {1},
+                                                            {1}};
 };
 
 #endif
