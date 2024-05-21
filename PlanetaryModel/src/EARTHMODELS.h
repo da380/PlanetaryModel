860349d99b41ed6895bbbdde8ab84bcf7c3cdb43
Alex Myhill <adcm2@cam.ac.uk>
2024-05-21 12:17:04 +0100

    With density

--- PlanetaryModel/src/EARTHMODELS.h
+++ PlanetaryModel/src/EARTHMODELS.h
@@ -374,7 +374,15 @@ class HOMOSPHERE : public HomogeneousConstants<FLOAT> {
 
     // Density
     Interpolation::Polynomial1D<FLOAT> Density(INTEGRAL i) const {
-        return 1000.0 * vec_density[i];
+        if (i < 0) {
+            Interpolation::Polynomial1D<FLOAT> rettemp{0.0};
+            return rettemp;
+        } else if (i > this->NumberOfLayers() - 1) {
+            Interpolation::Polynomial1D<FLOAT> rettemp{0.0};
+            return rettemp;
+        } else {
+            return 1000.0 * vec_density[i];
+        }
     };
 
     // Isotropy/fluid/solid etc
