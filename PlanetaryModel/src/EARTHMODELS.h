dcaac09d8f89c6e1c3de6bde8fb8237f25c093ca
Alex Myhill <adcm2@cam.ac.uk>
2024-06-04 13:55:54 +0100

    Pert8

--- PlanetaryModel/src/EARTHMODELS.h
+++ PlanetaryModel/src/EARTHMODELS.h
@@ -714,6 +714,32 @@ class HOMOBOUND7 : public HOMOSPHERE<FLOAT, int> {
         {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 };
 
+template <typename FLOAT = double, typename INTEGRAL = int>
+class HOMOBOUND8 : public HOMOSPHERE<FLOAT, int> {
+
+  public:
+    using size_type = INTEGRAL;
+
+    // Constructor
+    HOMOBOUND8(){};
+
+    // Density
+    Interpolation::Polynomial1D<FLOAT> DensityPerturbation(INTEGRAL i) {
+        return vec_pert_density[i];
+    };
+    // std::function<FLOAT(FLOAT, FLOAT, FLOAT)> RadialMap() const {
+    //     return RadialMap();
+    // };
+    FLOAT RadialMap(FLOAT r, FLOAT theta, FLOAT phi) const {
+        return 0.5 * r * (1 - r / this->OuterRadius()) * std::cos(theta);
+    };
+    FLOAT MaxRadius() const { return this->OuterRadius() * 1.0; };
+
+  private:
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_pert_density{
+        {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+};
+
 };   // namespace EarthModels
 
 #endif
