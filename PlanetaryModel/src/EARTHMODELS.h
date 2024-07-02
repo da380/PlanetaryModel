4794d471d0f6154487fd74203a8e215c3565f6aa
Alex Myhill <adcm2@cam.ac.uk>
2024-07-02 14:14:37 -0400

    Anothermodel

--- PlanetaryModel/src/EARTHMODELS.h
+++ PlanetaryModel/src/EARTHMODELS.h
@@ -767,6 +767,32 @@ class HOMOBOUND9 : public HOMOSPHERE<FLOAT, int> {
         {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 };
 
+template <typename FLOAT = double, typename INTEGRAL = int>
+class HOMOBOUND10 : public HOMOSPHERE<FLOAT, int> {
+
+  public:
+    using size_type = INTEGRAL;
+
+    // Constructor
+    HOMOBOUND10(){};
+
+    // Density
+    Interpolation::Polynomial1D<FLOAT> DensityPerturbation(INTEGRAL i) {
+        return vec_pert_density[i];
+    };
+    // std::function<FLOAT(FLOAT, FLOAT, FLOAT)> RadialMap() const {
+    //     return RadialMap();
+    // };
+    FLOAT RadialMap(FLOAT r, FLOAT theta, FLOAT phi) const {
+        return 0.02 * r * (1 - r / this->OuterRadius()) * std::cos(theta);
+    };
+    FLOAT MaxRadius() const { return this->OuterRadius() * 1.0; };
+
+  private:
+    std::vector<Interpolation::Polynomial1D<FLOAT>> vec_pert_density{
+        {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+};
+
 };   // namespace EarthModels
 
 #endif
